4ef07bdce1601b2589883cc3055aac4a8d6b28aa
gagomanuel <manuelmeg96@gmail.com>
2022-05-22 13:37:13 -0300

    modificado app.js, rutas en users

--- code/app.js
+++ code/app.js
@@ -3,32 +3,31 @@ const app = express()
 const port = 3000
 const path = require('path')
 
-//views 
-const views = path.join(__dirname, 'views/')
 //public
-const public = path.join(__dirname, 'public/')
-const home = path.join(__dirname, 'views/home.html')
-const carrito = path.join(__dirname, 'views/carritoDeCompras.html')
-const login = path.join(__dirname, 'views/login.html')
-const register = path.join(__dirname, 'views/register.html')
-
+    // const public = path.join(__dirname, 'public/')
+    const publicPath = path.resolve(__dirname,'./public');
+    app.use(express.static(publicPath));
+//views 
 //html
-const homehtml = 'home.html'
-const detalleDeProducto = path.join(__dirname, 'views/detalleDeProducto.html')
+const views = path.join(__dirname, 'views/')
+    const home = path.join(__dirname, 'views/users/home.html')
+    const carrito = path.join(__dirname, 'views/users/carritoDeCompras.html')
+    const login = path.join(__dirname, 'views/users/login.html')
+    const register = path.join(__dirname, 'views/users/register.html')
+    const detalleDeProducto = path.join(__dirname, 'views/users/detalleDeProducto.html')
+    const homehtml = 'home.html'
+
 //http
-const htpptRaiz = '/'
-const htpptHome = '/home'
-const htpptCarrito = '/carrito'
-const htpptLogin = '/login'
-const httpDetalleDeProducto = '/detalle'
-const htpptRegister = '/Register'
+    const htpptRaiz = '/'
+    const htpptHome = '/home'
+    const htpptCarrito = '/carrito'
+    const htpptLogin = '/login'
+    const httpDetalleDeProducto = '/detalle'
+    const htpptRegister = '/Register'
 
 
 
-const publicPath = path.resolve(__dirname,'./public');
-app.use(express.static(publicPath));
-
-//Definir los camions
+//Definir las rutas
 app.listen(port, () => console.log('Example app listening at http://localhost:'+port))
 
 app.get(htpptRaiz, function(req, res){
