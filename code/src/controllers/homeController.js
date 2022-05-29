9c0a94c20674bd12c2d30d27144f0db704f18ca1
belenkestingDurand <belenkesting@gmail.com>
2022-05-28 22:57:39 -0300

    cambios en el detalle del producto

--- code/src/controllers/homeController.js
+++ code/src/controllers/homeController.js
@@ -1,16 +1,64 @@
 // LUGAR PARA FUTURAS BASES DE DATOS
+const about = {
+                books: "Libros"
+                }
+const books = [{
+              id: 1,
+              name: "La Cabeza de Macri",
+              author: "Franco Linder",
+              price: "$4000.00",
+              picture: "/img/book-la-cabeza-de-macri.webp"},
 
+              {
+              id: 2,
+              name: "Matar un Ruiseñor",
+              author: "Harper Lee",
+              price: "$3260.00",
+              picture: "/img/book-matar-a-un-ruisenior.webp"},
+
+              {
+              id: 3,
+              name: "Red Queen",
+              author: "Victoria Avyard",
+              price: "$2600.00",
+              picture: "/img/book-red-queen.webp"},
+
+              {
+              id: 4,
+              name: "Harry Potter",
+              author: "J.K.Rowling",
+              price: "$5200.00",
+              picture: "/img/book-harry-potter.jpeg"},
+
+              {
+              id: 5,
+              name: "Las Vidas dentro De tu cabeza",
+              author: "Franco Linder",
+              price: "$5400.00",
+              picture: "/img/book-las-vidas-dentro-de-tu-cabeza.webp"},
+
+              {
+              id: 6,
+              name: "El Buen Cirujano",
+              author: "Ricardo Garcia",
+              price: "$7200.00",
+              picture: "/img/book-el-buen-cirujano.webp"},
+              
+            ]
 
 // OBJECT WITH DETAILED HTML DIRECTIONS
 const homeController =  {
     //'home.ejs' IN 'views/products' FOLDER
     home: (req, res) => {
-        res.render('products/home')
+        res.render('products/home',{about: about, books: books})
     },
     //'detalleDeProducto.ejs' IN 'views/products' FOLDER
     detalle: (req,res) => {
-        res.render('products/detalleDeProducto')
+        let product = books.find(product => product.id == req.params.bookId)
+        console.log(product)
+        res.render('products/detalleDeProducto',{product: product})
     },
+
     //'carritoDeCompras.ejs' IN 'views/prodcuts' FOLDER
     carrito: (req,res) => {
         res.render('products/carritoDeCompras')
