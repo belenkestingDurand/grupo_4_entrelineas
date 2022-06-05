2f4c5f1df46c6c96a12e1d482b1ac1f5443e7269
belenkestingDurand <belenkesting@gmail.com>
2022-06-05 19:12:46 -0300

    Agregamos las rutas parametrizadas

--- code/src/controllers/homeController.js
+++ code/src/controllers/homeController.js
@@ -2,63 +2,21 @@
 const about = {
                 books: "Libros"
                 }
-const books = [{
-              id: 1,
-              type: 'Libro',
-              name: "La Cabeza de Macri",
-              gender:"Biografía",
-              author: "Franco Linder",
-              price: "$4000.00",
-              picture: "/img/book-la-cabeza-de-macri.webp",
-              opinion: "Este es un libro ... que tiene ... muy interesante..."},
-
-              {
-              id: 2,
-              name: "Matar un Ruiseñor",
-              author: "Harper Lee",
-              price: "$3260.00",
-              picture: "/img/book-matar-a-un-ruisenior.webp"},
-
-              {
-              id: 3,
-              name: "Red Queen",
-              author: "Victoria Avyard",
-              price: "$2600.00",
-              picture: "/img/book-red-queen.webp"},
-
-              {
-              id: 4,
-              name: "Harry Potter",
-              author: "J.K.Rowling",
-              price: "$5200.00",
-              picture: "/img/book-harry-potter.jpeg"},
-
-              {
-              id: 5,
-              name: "Las Vidas dentro De tu cabeza",
-              author: "Franco Linder",
-              price: "$5400.00",
-              picture: "/img/book-las-vidas-dentro-de-tu-cabeza.webp"},
-
-              {
-              id: 6,
-              name: "El Buen Cirujano",
-              author: "Ricardo Garcia",
-              price: "$7200.00",
-              picture: "/img/book-el-buen-cirujano.webp"},
-              
-            ]
+const fs = require("fs")
 
 // OBJECT WITH DETAILED HTML DIRECTIONS
 const homeController =  {
     //'home.ejs' IN 'views/products' FOLDER
     home: (req, res) => {
+        let datos = fs.readFileSync('./data/products.json')
+        let books = JSON.parse(datos)
         res.render('products/home',{about: about, books: books})
     },
     //'detalleDeProducto.ejs' IN 'views/products' FOLDER
     detalle: (req,res) => {
+        let datos = fs.readFileSync('./data/products.json')
+        let books = JSON.parse(datos)
         let product = books.find(product => product.id == req.params.bookId)
-        console.log(product)
         res.render('products/detalleDeProducto',{product: product})
     },
 
