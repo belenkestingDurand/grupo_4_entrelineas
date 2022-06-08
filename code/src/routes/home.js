5edb2b74e5a2287697264d22cb4f5e17fe1ea0e4
gagomanuel <manuelmeg96@gmail.com>
2022-06-08 17:37:41 -0300

    routes/home con comentarios para empezar a armar los CRUD
    va a haber que cambiar las direcciones de varias vistas para que sigan coincidiendo

--- code/src/routes/home.js
+++ code/src/routes/home.js
@@ -5,15 +5,33 @@ const router = express.Router()
 // CONTROLLER
 const homeCtrl = require('../controllers/homeController')
 
-
+//- HOME PAGE
 router.get('/',homeCtrl.home)
 router.get('/home',homeCtrl.home)
-router.get('/detalle/:bookId', homeCtrl.detalle)
+
+//- PRODUCT DETAIL
+router.get('/detalle/:bookId', homeCtrl.detalle) // cambiar a /products/:bookId
 //router.get('/detalle', homeCtrl.detalle)
+
+//- CARRITO
 router.get('/carrito', homeCtrl.carrito)
-router.get('/crearProducto', homeCtrl.crearProducto)
+
+
+
+
+//* USER: ADMIN
 router.get('/listarProducto', homeCtrl.listarProducto)
-router.get('/editarProducto/', homeCtrl.editarProducto)
+
+//- CREATE PRODUCT 
+router.get('/crearProducto', homeCtrl.crearProducto) // cambiar a /products/create"
+// router.post('/listarProducto', homeCtrl.{producto creado})
+
+// - EDIT PRODUCT
+router.get('/editarProducto', homeCtrl.editarProducto) // cambiar a /products/:id/edit
+// router.put('/listarProducto', homeCtrl.{producto editado})
+
+//- DELETE PRODUCT
+// router.delete('/listarProducto', homeCtrl.{producto borrado})
 
 // exports
 module.exports = router
