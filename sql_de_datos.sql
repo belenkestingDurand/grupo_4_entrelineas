3620f68f25ae151b5f6c28f9a074f098529a4e7c
gagomanuel <manuelmeg96@gmail.com>
2022-07-19 12:06:12 -0300

    6. 1er commit de sql_datos + cambios en users.json

---
+++ sql_de_datos.sql
@@ -0,0 +1,14 @@
+-- LOCK TABLES `actor_episode` WRITE;
+-- UNLOCK TABLES;
+-- INSERT INTO `actors` VALUES (1,NULL,NULL,'Sam','Worthington',7.5,1),(2,NULL,NULL,'Zoe','Saldana',5.5,2),
+
+-- Data for table 'usercategory'
+INSERT INTO entrelineas.usercategory VALUES
+(1,'user'),(2,'admin');
+
+
+-- Data for table 'users' 
+INSERT INTO entrelineas.users VALUES 
+(1,'María Isolina','Durand Figueroa','isolinadurand@gmail.com','$2a$10$hp5ztx.IxeN1Snf29ic.dOUUDJIbNov4Qx1Vc2oJT32rj/oIWIzJG','img-1656894481277-785793624.jpg',1),
+(2,'Manuel','Gago','manuelmeg96@gmail.com','$2a$10$u76NAPtWZbuvPTVjXHJtke6L0wy5gi0/cGRYphp0xu3f98AtswuGS','img-1656894481277-785793624.jpg',1),
+(3,'Bronco','Kesting','bronco@gmail.com','$2a$10$uN/ndZg8axVVBagbEukWNO1ft4VNzn0GivFuOJvHxphX0r2T5nH76','img-1657195760992-497204755.jpg',1)
