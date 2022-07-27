aece63fc7c95124065d3e99c8d937c4ba6a51136
belen <belen@kesting.com>
2022-07-27 20:38:17 -0300

    Creo el primero modelo

---
+++ code/src/database/models/usersCategory.js
@@ -0,0 +1,26 @@
+
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = 'UserCategory';
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER,
+        },
+        name: { 
+            type: dataTypes.STRING(100),
+        }
+    };
+    let config = {
+        tableName: "userCategory"
+    
+    };
+    
+    const UserCategory = sequelize.define(alias, cols, config);
+
+    UserCategory.hasMany(Users, {as: "users", foreignKey: "id_userCategory"})
+    
+    return UserCategory;
+    }
+
