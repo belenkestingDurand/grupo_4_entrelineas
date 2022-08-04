9bc96695b28f00f52f9ed8064f44ac745798729a
belen <belen@kesting.com>
2022-08-04 00:51:44 -0300

    correcion del script de datos

--- code/src/database/config/config.js
+++ code/src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": '42130851',
     "database": "entrelineas",
     "host": "127.0.0.1",
     "dialect": "mysql"
